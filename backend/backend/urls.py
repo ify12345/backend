985049f7433c93fc2c1d5e0e7520e8ef0d27622f
Ifeanyi <ifeanyiuchenwakodo@gmail.com>
2026-06-10 03:29:24 -0500

    Production-ready Django Notes API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
